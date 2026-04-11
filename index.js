922db78c075931cb97321812e0f058a8660a1fe8
gstcopilot5 <gstcopilot@gmail.com>
2026-04-11 19:37:29 +0000

    fix route

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const PORT = process.env.PORT || 3000;
 
 fastify.get("/", async (request, reply) => {
   reply.send({ status: "working" });
-});
-
-fastify.listen({ port: PORT, host: "0.0.0.0" })
-  .then(() => console.log("Server running"))
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
   });
+
+  fastify.listen({ port: PORT, host: "0.0.0.0" })
+    .then(() => console.log("Server running"))
+      .catch(err => {
+          console.error(err);
+              process.exit(1);
+                });
