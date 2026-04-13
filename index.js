bd7de5f3439ffe61a8de11f2ed31d0e89af5b8b0
gstcopilot5 <gstcopilot@gmail.com>
2026-04-13 11:03:01 +0000

    remove deleted org.js reference

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express=require('express');
 const app=express();
 app.use(express.json());
-app.use('/org',require('./routes/org'));
 app.use('/org',require('./routes/orgs'));
 app.use('/agents',require('./routes/agents'));
 app.use('/policies',require('./routes/policies'));
